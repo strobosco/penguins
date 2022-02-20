e74d88dc2d94c1f686452c99004aa1b420ea8c4d
strobosco <54740672+strobosco@users.noreply.github.com>
2022-02-19 23:19:06 -0500

    added sample post card

---
+++ src/components/posts.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+import photo from "../images/1.jpg";
+
+const Posts = () => {
+  return (
+    <div className="posts">
+      <img src={photo} />
+    </div>
+  );
+};
+
+export default Posts;
