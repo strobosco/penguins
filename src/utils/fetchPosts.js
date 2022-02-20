2bf67474bb78ce5f58d50ead7051634f2e6ec6b5
strobosco <54740672+strobosco@users.noreply.github.com>
2022-02-20 18:12:01 -0500

    added post fetching

--- src/utils/fetchPosts.js
+++ src/utils/fetchPosts.js
@@ -1,5 +1,28 @@
 import axios from "axios";
 
+const API_URL = "https://api.giphy.com/v1/gifs/search";
 const API_KEY = "WL8y7AaUtu88Nq9yAPU9PnsLQqWMN8DO";
 
-const fetchPosts = () => {};
+export const fetchPosts = async () => {
+  const res = await axios
+    .get(API_URL, {
+      params: {
+        api_key: API_KEY,
+        q: "penguins",
+        limit: 16,
+      },
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+
+  const posts = res.data.data;
+  const links = new Array();
+  posts.map((p) => {
+    links.push(p.images.original_mp4.mp4);
+  });
+  console.log(links);
+  /**
+   * Links store the top 20 results for 'penguins' using the Giphy API
+   */
+};
